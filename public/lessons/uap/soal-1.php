bbd02164a296608bb989de6c63f2a592ad8c7e27
adityapryg <aditya@brainmatics.com>
2022-08-26 21:56:02 +0700

    [ADD] Soal nomor 1

--- public/lessons/uap/soal-1.php
+++ public/lessons/uap/soal-1.php
@@ -1,1 +1,39 @@
-<?= 'Hallo' ?>
+<?php
+    $array_data = [
+        "npm"   => 201943502046,
+        "nama"  => "ADITYA PRAYOGA"
+    ];
+    
+    $npm = str_split($array_data['npm']);
+    $jml_kolom = array_pop($npm);
+    $jml_baris = array_pop($npm);
+?>
+
+<!doctype html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Membuat Tabel NPM dan Mahasiswa</title>
+
+    <style>
+        table, th, td {
+            border:1px solid black;
+            font-size: small;
+        }
+    </style>
+</head>
+<body>
+    <table>
+        <?php for($i = ($jml_baris ?: 1); $i >= 1; $i--): ?>
+            <tr >
+                <?php for($j = ($jml_kolom ?: 1); $j >= 1; $j--): ?>
+                    <td style="background-color: <?= $j % 2 ? "blue" : "yellow" ?>">NPM: <?= $array_data["npm"] ?> Nama: <?= $array_data["nama"] ?></td>
+                <?php endfor; ?>
+            </tr>
+        <?php endfor; ?>
+    </table>
+</body>
+</html>
