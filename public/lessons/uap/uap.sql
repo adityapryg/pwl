b14eaaa11564bd1c23bc802b60276373d8017b9e
adityapryg <aditya@brainmatics.com>
2022-08-26 22:26:28 +0700

    [FIX] 2. file sql

--- public/lessons/uap/uap.sql
+++ public/lessons/uap/uap.sql
@@ -1,4 +1,16 @@
 CREATE DATABASE db_uap2046;
 USE db_uap2046;
-CREATE TABLE table_calon_mhs ( kode_calon_mhs varchar(5), nama_calon_mhs varchar(30), alamat text, telp varchar(15) );
-CREATE TABLE table_seleksi ( kode_seleksi varchar(5), kode_calon_mhs varchar(5), nilai_tkda float(2), nilai_matematika float(2), nilai_wawancara float(2), rata_rata float(2) );
+CREATE TABLE table_calon_mhs ( 
+    kode_calon_mhs varchar(5), 
+    nama_calon_mhs varchar(30), 
+    alamat text, 
+    telp varchar(15) 
+);
+CREATE TABLE table_seleksi ( 
+    kode_seleksi varchar(5), 
+    kode_calon_mhs varchar(5), 
+    nilai_tkda float(2), 
+    nilai_matematika float(2), 
+    nilai_wawancara float(2), 
+    rata_rata float(2) 
+);
