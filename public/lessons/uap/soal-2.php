73142f41dd6715f90d81493857e6e8450dce7477
adityapryg <aditya@brainmatics.com>
2022-08-27 01:04:02 +0700

    [RFR] Change kode mahasiswa to 12 digit NPM

--- public/lessons/uap/soal-2.php
+++ public/lessons/uap/soal-2.php
@@ -19,7 +19,7 @@
         <li>CREATE DATABASE db_uap2046;</li>
         <li>USE db_uap2046;</li>
         <li>CREATE TABLE table_calon_mhs (
-                kode_calon_mhs varchar(5), 
+                kode_calon_mhs varchar(12), 
                 nama_calon_mhs varchar(30),
                 alamat text,
                 telp varchar(15)
@@ -27,7 +27,7 @@
         </li>
         <li>CREATE TABLE table_seleksi (
                 kode_seleksi varchar(5), 
-                kode_calon_mhs varchar(5),
+                kode_calon_mhs varchar(12),
                 nilai_tkda float(2),
                 nilai_matematika float(2),
                 nilai_wawancara float(2),
